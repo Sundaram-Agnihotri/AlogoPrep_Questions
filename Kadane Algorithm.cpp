d96ea35f7cf71d4bf00fe329eb23a13b8971906c
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-12-25 00:07:41 +0530

    Day 1

---
+++ Kadane Algorithm.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+     int maxSubArray(vector<int>& arr) {
+        int n=arr.size();
+
+        int maxi=arr[0];
+          int sum=arr[0];
+
+        for(int i=1;i<n;i++){
+           if(sum<0)
+           sum=arr[i];
+
+           else 
+           sum+=arr[i];
+           
+           maxi=max(sum,maxi);
+        }
+        return maxi;
+    }
+};
